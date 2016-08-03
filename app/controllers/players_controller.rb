dcd63e3edb8c5a011ae768da848bc5c1103ee895
Christopher Soden <karavol27@gmail.com>
2016-08-02 18:58:12 -0600

    Add styling and move dashboard script

---
+++ app/controllers/players_controller.rb
@@ -0,0 +1,7 @@
+class PlayersController < ApplicationController
+  def create
+  end
+
+  def update
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'players/create'
+
+  get 'players/update'
+
   root 'pages#index'
 
   get 'auth/facebook', as: :facebook_login
@@ -12,6 +16,4 @@ Rails.application.routes.draw do
   get 'roster/:id', to: 'teams#index', as: :roster
   post 'teams/create', to: 'teams#create', as: :team_create
   get 'twilio/welcome_messages', to: 'twilio#create', as: :welcome_messages
-
-
 end
