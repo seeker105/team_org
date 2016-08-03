1536440f68a29f5aea0a2fce9a0f7b03e3cb732d
Christopher Soden <karavol27@gmail.com>
2016-08-03 09:52:05 -0600

    Now goes from root through OAuth to dashboard. Before deleting landing page code

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_or_create_from_auth_hash(auth_hash)
     session[:user_id] = user.id
-    redirect_to landing_page_path
+    redirect_to coach_dashboard_path
   end
 
   private
