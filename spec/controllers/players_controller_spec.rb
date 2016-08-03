4f7ff859d74f7f0de106a374aa71ca42c0624778
Christopher Soden <karavol27@gmail.com>
2016-08-02 19:33:39 -0600

    Disable autocreated controller tests

--- spec/controllers/players_controller_spec.rb
+++ spec/controllers/players_controller_spec.rb
@@ -3,17 +3,17 @@ require 'rails_helper'
 RSpec.describe PlayersController, type: :controller do
 
   describe "GET #create" do
-    it "returns http success" do
-      get :create
-      expect(response).to have_http_status(:success)
-    end
+    # it "returns http success" do
+    #   get :create
+    #   expect(response).to have_http_status(:success)
+    # end
   end
 
   describe "GET #update" do
-    it "returns http success" do
-      get :update
-      expect(response).to have_http_status(:success)
-    end
+    # it "returns http success" do
+    #   get :update
+    #   expect(response).to have_http_status(:success)
+    # end
   end
 
 end
