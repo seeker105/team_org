ead3f87cc1f83cab16b72e52ba0e73ac5c5fd29b
Christopher Soden <karavol27@gmail.com>
2016-08-03 23:41:12 -0600

    Minor edit

--- app/services/twilio_service.rb
+++ app/services/twilio_service.rb
@@ -9,7 +9,7 @@ class TwilioService
       @client.account.messages.create({
         :from => '+13037207243',
         :to => player.mobile_number,
-        :body => "Welcome #{player.full_name}! You have been added to the #{team.name}!",
+        :body => "Welcome #{player.full_name}! You have been added to the #{team.name} team!",
       })
     end
   end
